3fc9a3f010a706fb3dc2a40e48a4a5178fe537f5
JlXip <jlxip@protonmail.com>
2020-06-06 21:33:09 +0200

    Next text

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 int main() {
 	std::cout << "Hello! This is just a test." << std::endl;
 	std::cout << "Another line." << std::endl;
+	std::cout << "Last one last one." << std::endl;
 
 	return 0;
 }
